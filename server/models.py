28576a3b8032c0602e614f26dc686bc2b39c8373
lkbrian <lkbrian742@gmail.com>
2024-04-13 15:11:59 +0300

    Model creation

--- server/models.py
+++ server/models.py
@@ -1,14 +1,49 @@
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates
 
 from config import db, bcrypt
 
-class User(db.Model, SerializerMixin):
-    __tablename__ = 'users'
 
-    pass
+class User(db.Model, SerializerMixin):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True,nullable=False)
+    _password_hash = db.Column(db.String, nullable=False)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
+
+    recipes = db.relationship("Recipe",back_populates="user", cascade="all")
+
+    def __repr__(self):
+        return f"User {self.username}, ID {self.id}"
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('Password hash is not readable')
+        return self._password_hash
+
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(password.encode("utf-8"))
+        self._password_hash = password_hash.decode("utf-8")
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode("utf-8"))
+
 
 class Recipe(db.Model, SerializerMixin):
-    __tablename__ = 'recipes'
-    
-    pass
+    __tablename__ = "recipes"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String,nullable=False)
+    instructions = db.Column(db.String, nullable=False)
+    minutes_to_complete = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    user = db.relationship("User",back_populates="recipes")
+
+    @validates("instructions")
+    def validates_instructions(self, key, instructions):
+        if len(instructions) >= 50:
+            return instructions
+        print("instructions must atleast 50 characters long")
